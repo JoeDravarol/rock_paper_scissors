5ce878c7df617908c64dc0bd8f4e23cac163b296
Joe <joedravarol@gmail.com>
2019-04-11 23:58:56 +0100

    Modify code and add event listener to buttons

--- script.js
+++ script.js
@@ -1,79 +1,67 @@
-let playerSelection, computerSelection;
-    let playerScore = 0;
-    let aiScore = 0;
+let playerScore = 0;
+let aiScore = 0;
 
-    function computerPlay() {
-        const choice = Math.floor(Math.random() * 3);
-        
-        if (choice === 0) {
-            return 'rock';
-        } else if (choice === 1) {
-            return 'paper';
-        } else {
-            return 'scissors';
-        }
-    };
+function computerPlay() {
+    const choice = Math.floor(Math.random() * 3);
 
-    function playRound(playerSelection, computerSelection) {
-        // Making the input to lower case to no worry about value being case sensitive later
-        const p1 = playerSelection.toLowerCase();
-        const ai = computerSelection;
+    if (choice === 0) {
+        return 'rock';
+    } else if (choice === 1) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+};
 
-        // Draw condition
-        if (p1 === ai ) {
-            playerScore++;
-            aiScore++;
-            return "It's a draw!";
+function playRound(e) {
+    const ai = computerPlay();
+    let p1;
 
-            // Rock win condition
-        } else if (p1 === 'rock') {
-            if (ai === 'scissors') {
-                playerScore++;
-                return `You win! ${p1} beats ${ai}`;
-            } else {
-                aiScore++;
-                return `You lose! ${ai} beats ${p1}`;
-            }
-            // Paper win condition
-        } else if (p1 === 'paper') {
-            if (ai === 'rock') {
-                playerScore++;
-                return `You win! ${p1} beats ${ai}`;
-            } else {
-                aiScore++;
-                return `You lose! ${ai} beats ${p1}`;
-            }
-            // Scissors win condition
-        } else {
-            if (ai === 'paper') {
-                playerScore++;
-                return `You win! ${p1} beats ${ai}`;
-            } else {
-                aiScore++
-                return `You lose! ${ai} beats ${p1}`;
-            }
-        }
-    };
-
-    function getSelection() {
-        playerSelection = prompt("Type either Rock, Paper or Scissors");
-        computerSelection = computerPlay();
+    // If there's no ID the travese the DOM to find an id
+    if (e.target.id) {
+        p1 = e.target.id;
+        console.log(p1)
+    } else {
+        p1 = e.target.parentElement.id;
     }
 
-    function game() {
-
-        for (let i = 0; i < 5; i++) {
-            getSelection();
-            console.log(playRound(playerSelection, computerSelection));
-        }
+    // Draw condition
+    if (p1 === ai) {
+        return "It's a draw!";
         
-        if (playerScore > aiScore) {
-            console.log(`The player has won with ${playerScore} score against the ai with ${aiScore} score`);
-        } else if (playerScore < aiScore) {
-            console.log(`The ai has won with ${aiScore} score against the player with ${playerScore} score`);
+        // Rock win condition
+    } else if (p1 === 'rock') {
+        if (ai === 'scissors') {
+            playerScore++;
+            return `You win! ${p1} beats ${ai}`;
         } else {
-            console.log(`It's a draw between both player with ${playerScore} score each`);
+            aiScore++;
+            return `You lose! ${ai} beats ${p1}`;
         }
-    };
+        // Paper win condition
+    } else if (p1 === 'paper') {
+        if (ai === 'rock') {
+            playerScore++;
+            return `You win! ${p1} beats ${ai}`;
+        } else {
+            aiScore++;
+            return `You lose! ${ai} beats ${p1}`;
+        }
+        // Scissors win condition
+    } else if (p1 === "scissors") {
+        if (ai === 'paper') {
+            playerScore++;
+            return `You win! ${p1} beats ${ai}`;
+        } else {
+            aiScore++
+            return `You lose! ${ai} beats ${p1}`;
+        }
+    }
+};
 
-    game();
+const playerSelections = document.querySelectorAll('.btn');
+
+playerSelections.forEach(selection => {
+    selection.addEventListener('click', playRound);
+})
+
